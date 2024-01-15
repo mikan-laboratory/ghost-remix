18a6b263ea30adb4d0fc89b640a7777844329d43
jakovid <jacobk.dowdle@gmail.com>
2024-01-15 13:01:58 -0500

    renamed a variable

--- app/content-api/getSearchResults.ts
+++ app/content-api/getSearchResults.ts
@@ -1,13 +1,13 @@
 import { ghostContentAPI } from './ghostContentAPI';
 
 export const getSearchResults = async (query: string = '', page = 1, limit = 5) => {
-  const tagsList = query.toLowerCase().replaceAll(' ', ',');
+  const queryList = query.toLowerCase().replaceAll(' ', ',');
   //Ghost API can't do partial/like filter with the current state of NQL, so this is a work around.
   let queryWords = query.split(' ');
   let transformedWords = queryWords.map((word) => `title:~'${word}'`);
   let titleList = transformedWords.join(', ');
 
-  const filterQuery = `${titleList}, tags: [${tagsList}], authors: [${tagsList}]`;
+  const filterQuery = `${titleList}, tags: [${queryList}], authors: [${queryList}]`;
   const posts = await ghostContentAPI.posts.browse({
     limit: limit,
     page: page,
