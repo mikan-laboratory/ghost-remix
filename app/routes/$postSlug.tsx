1bb2dabf70b1ac1d543f7d64b3b5827e3e99f120
jakovid <jacobk.dowdle@gmail.com>
2024-01-16 21:32:46 -0500

    started comments section

--- app/routes/$postSlug.tsx
+++ app/routes/$postSlug.tsx
@@ -62,6 +62,7 @@ export default function Post() {
       <Box py={5} textColor="text2">
         <PostContent html={postHtml} />
       </Box>
+      {post.comments && <Box>comments</Box>}
     </Box>
   );
 }
