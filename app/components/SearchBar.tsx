b24a8b9384f6fac243cf32e7d6fdc7d9d9c29599
Keenan Thompson <keenan.thompson67@gmail.com>
2024-06-26 17:35:21 +0800

    main: search bar styles

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,12 +1,10 @@
-//External Library Imports
-import { Flex, Input, Button, Stack, useMediaQuery } from '@chakra-ui/react';
+import { Flex, Input, Button, Stack } from '@chakra-ui/react';
 import { Form } from '@remix-run/react';
 import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
 export default function SearchBar() {
   const [query, setQuery] = useState('');
-  const [isSmallScreen] = useMediaQuery('(max-width: 600px)');
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     if (!query.trim()) {
@@ -15,40 +13,50 @@ export default function SearchBar() {
   };
 
   return (
-    <Flex display="flex">
-      <Form method="get" action="/search" style={{ width: '20em' }} onSubmit={handleSubmit}>
-        <Stack direction="row" w="100%" gap="0">
+    <Flex display="flex" p={0} m={0}>
+      <Form method="get" action="/search" style={{ width: '20em', margin: 0, padding: 0 }} onSubmit={handleSubmit}>
+        <Stack
+          direction="row"
+          w="100%"
+          gap="0"
+          backgroundColor="white"
+          borderRadius="10px"
+          borderWidth="2px"
+          _focusWithin={{ borderColor: 'secondary' }}
+          _hover={{ borderColor: 'secondary' }}
+          p={0}
+          m={0}
+        >
           <Input
             type="search"
+            color="text"
             name="query"
             placeholder="Search..."
-            borderColor="primary"
             borderEndRadius="0"
-            borderWidth="2px"
-            backgroundColor="background"
-            _hover={{ borderColor: 'secondary' }}
-            focusBorderColor="secondary"
+            border="none"
+            backgroundColor="white"
+            focusBorderColor="transparent"
             textColor="gray.700"
-            fontSize="md"
+            fontSize="sm"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
           />
           <Button
             type="submit"
-            background="secondary"
-            textColor="background"
-            borderColor="secondary"
-            borderWidth="2px"
-            borderLeft="0px"
+            color="text2"
+            background="none"
+            border="none"
             fontSize="md"
             borderStartRadius="0"
             _hover={{
-              background: 'background',
-              color: 'secondary',
+              background: 'primary',
+              color: 'background',
             }}
             _focus={{
               boxShadow: '0 0 0 3px rgba(66, 153, 225, 0.6)',
             }}
+            p={0}
+            m={0}
           >
             <FaSearch />
           </Button>
