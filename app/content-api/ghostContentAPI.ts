dd403b068fc84eb4eee616da7831db608a2db499
Keenan Thompson <keenan.thompson67@gmail.com>
2024-07-08 20:07:51 +0800

    Revert "dev: remove GHOST_CONTENT_API_KEY"
    This reverts commit fce6ed3ca2bc833af0f24cac67f8b7587323e140.

---
+++ app/content-api/ghostContentAPI.ts
@@ -0,0 +1,9 @@
+import GhostContentAPI from '@tryghost/content-api';
+
+import { env } from '~/env';
+
+export const ghostContentAPI = new GhostContentAPI({
+  url: env.GHOST_URL,
+  key: env.GHOST_CONTENT_API_KEY,
+  version: 'v5.0',
+});
--- app/env.ts
+++ app/env.ts
@@ -5,6 +5,7 @@ export const env = createEnv({
   clientPrefix: 'PUBLIC_',
   server: {
     GHOST_URL: z.string().min(1),
+    GHOST_CONTENT_API_KEY: z.string().min(1),
     JWT_SECRET: z.string().min(1),
     ENVIRONMENT: z.enum(['local', 'production']).optional(),
     ANTHROPIC_API_KEY: z.string().optional(),
