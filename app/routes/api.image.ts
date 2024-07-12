00efe44c48fa7c1c130f117743536786246fdbfe
Keenan Thompson <keenan.thompson67@gmail.com>
2024-07-13 01:14:36 +0800

    feature-responsive-images: console.error

--- app/routes/api.image.ts
+++ app/routes/api.image.ts
@@ -39,7 +39,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       },
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
 
     throw new Response('Something went wrong', {
       status: 500,
