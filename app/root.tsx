9eb4bc0b4d0246c4bc71631e14aa2d48f40abeb4
Keenan Thompson <keenan.thompson67@gmail.com>
2024-07-14 02:47:23 +0800

    fix-char-mismatch: meta

--- app/root.tsx
+++ app/root.tsx
@@ -53,7 +53,7 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache) =>
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
